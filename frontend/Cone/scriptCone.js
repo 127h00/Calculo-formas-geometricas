143ca510f9c1d6c122b6ccce607051c42a4ba538
anna clara <128912768+127h00@users.noreply.github.com>
2024-06-02 19:30:41 -0300

    base mais ou menos de como conectar api e html

---
+++ frontend/Cone/scriptCone.js
@@ -0,0 +1,12 @@
+
+
+url -> da api com as rotas
+
+function async calcular () {
+    request = await fetch(url, data)
+    ret = request.json()
+    queryselector(#resposta).innerHTML = print print print
+
+    // nesse caso ai eu vou fazer varias requets
+    // uma pra cada troço que eu quero 
+}
